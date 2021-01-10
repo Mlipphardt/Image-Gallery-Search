522fb0b22e1e541ead203375f0f61a449891b380
Mlipphardt <matthewlipphardt@gmail.com>
2021-01-09 22:07:25 -0500

    Fetch images complete, images populate, start of banner styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,41 @@
-const baseURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&format=json`;
-
 const $ = document.querySelector.bind(document);
 
+//Image search function
 const getImages = async (text) => {
   try {
-    console.log("Retrieving images...");
-    url = `${baseURL}&text=${text}`;
+    text = text ? text : "cat";
+    const baseURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&format=json&nojsoncallback=1&safe_search=1`;
+    let url = `${baseURL}&text=${text}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let photos =
+      data.photos.photo.length > 20
+        ? data.photos.photo.slice(0, 20)
+        : data.photos.photo;
+    populateImages(photos);
   } catch (err) {
-    console.log(err);
+    console.warn(err);
   }
 };
 
-getImages();
+//Takes in an array of images from Flickr, creates img elements for each photo in array.
+const populateImages = (imageArray) => {
+  try {
+    const imgURL = "https://live.staticflickr.com/";
+    imageArray.forEach((photo) => {
+      let img = document.createElement("img");
+      let url = `${imgURL}${photo.server}/${photo.id}_${photo.secret}.jpg`;
+      img.setAttribute("src", url);
+      img.setAttribute("alt", photo.title ? photo.title : "Search result");
+      $("#image-gallery-wrapper").append(img);
+    });
+  } catch (err) {
+    console.warn(err);
+  }
+};
 
-console.log($("#search-btn"));
-
+//When search button clicked, trigger API get request.
 $("#search-btn").addEventListener("click", () => {
   let text = $("#search-input").value;
-  console.log(`Searching for ${text}...`);
+  getImages(text);
 });
